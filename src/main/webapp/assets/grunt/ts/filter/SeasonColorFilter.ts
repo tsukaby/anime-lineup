593c2fe1ebe619d740dbf7b0111a091bc22fdf8c
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-04-03 22:41:52 +0900

    Fix season type. string to number.

--- src/main/webapp/assets/grunt/ts/filter/SeasonColorFilter.ts
+++ src/main/webapp/assets/grunt/ts/filter/SeasonColorFilter.ts
@@ -2,14 +2,14 @@ module Filter {
     "use strict";
 
     export function SeasonColorFilter() {
-        return (input:any):any => {
-            if (input === "winter") {
+        return (input:number):string => {
+            if (input === 1) {
                 return "#5141D9";
-            } else if (input === "spring") {
+            } else if (input === 2) {
                 return "#B2F63D";
-            } else if (input === "summer") {
+            } else if (input === 3) {
                 return "#FF4540";
-            } else if (input === "autumn") {
+            } else if (input === 4) {
                 return "#FFAC40";
             } else {
                 return "#000000";
