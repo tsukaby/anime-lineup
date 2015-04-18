0afa2ebe48540e354e4124fa8df8f8a7b3be1ed8
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-04-18 21:06:04 +0900

    Update build process.

--- package_for_elastic_beanstalk.sh
+++ package_for_elastic_beanstalk.sh
@@ -2,15 +2,5 @@
 
 skinny package:standalone
 
-rm -rf elastic_beanstalk
-mkdir -p elastic_beanstalk/standalone-build/target/scala-2.11
-
-cp standalone-build/target/scala-2.11/anime-lineup* elastic_beanstalk/standalone-build/target/scala-2.11/
-cp Dockerrun.aws.json elastic_beanstalk/
-sed -e "s/MY_DD_API_KEY/$MY_DD_API_KEY/g" ./Dockerfile > elastic_beanstalk/Dockerfile
-
-cd elastic_beanstalk
-zip -r ../app ./*
-cd ..
-
-rm -rf elastic_beanstalk
+cp standalone-build/target/scala-2.11/anime-lineup* docker/anime-lineup.jar
+#sed -e "s/MY_DD_API_KEY/$MY_DD_API_KEY/g" ./Dockerfile > elastic_beanstalk/Dockerfile
