aeae1aebd2b55bee7dfdb82bbc02b76b6d81e624
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2015-04-28 08:19:11 +0900

    Update skinny.

--- project/Build.scala
+++ project/Build.scala
@@ -16,10 +16,10 @@ object SkinnyAppBuild extends Build {
   val appName = "anime-lineup"
   val appVersion = "0.1.0"
 
-  val skinnyVersion = "1.3.14"
-  val scalatraVersion = "2.3.0"
-  val theScalaVersion = "2.11.5"
-  val jettyVersion = "9.2.1.v20140609" // latest: "9.2.9.v20150224"
+  val skinnyVersion = "1.3.17"
+  val scalatraVersion = "2.3.1"
+  val theScalaVersion = "2.11.6"
+  val jettyVersion = "9.2.10.v20150310"
 
   lazy val baseSettings = ScalatraPlugin.scalatraWithJRebel ++ herokuSettings ++ Seq(
     organization := appOrganization,
@@ -37,10 +37,10 @@ object SkinnyAppBuild extends Build {
       "org.skinny-framework" %% "skinny-task" % skinnyVersion,
       "org.skinny-framework" %% "skinny-oauth2-controller" % skinnyVersion,
       "org.skinny-framework" %% "skinny-twitter-controller" % skinnyVersion,
-      "org.skinny-framework" % "skinny-logback" % "1.0.3",
-      "org.apache.commons" % "commons-dbcp2" % "2.0.1",
+      "org.skinny-framework" % "skinny-logback" % "1.0.6",
+      "org.apache.commons" % "commons-dbcp2" % "2.1",
       "mysql" % "mysql-connector-java" % "5.1.34",
-      "com.h2database" % "h2" % "1.4.185", // your own JDBC driver
+      "com.h2database" % "h2" % "1.4.187", // your own JDBC driver
       "ch.qos.logback" % "logback-classic" % "1.1.2",
       "org.skinny-framework" %% "skinny-factory-girl" % skinnyVersion % "test",
       "org.skinny-framework" %% "skinny-test" % skinnyVersion % "test",
